55a374a26318d4be6595f6df81b9e9c546b46b37
junrybuenavistahtgrp <90173944+junrybuenavistahtgrp@users.noreply.github.com>
2023-09-14 16:14:18 +0900

    aa

--- latency/outbound.php
+++ latency/outbound.php
@@ -1,11 +1,17 @@
 <?php
-
-$mac = $_REQUEST["mac"];
+//B4-2E-99-07-4E-47
+	error_reporting(0);
+	$macs = json_decode($_REQUEST["mac"]);
 	$values = array();
-	$lines = file('ping/'.$mac.'.txt');
-	$count = 0;
-	foreach($lines as $line) {
-		array_push($values,$line);
+	//print_r($macs);
+	
+	foreach($macs as $mac){
+		$lines = file('ping/'.$mac[1].'.txt');
+			
+		if($lines!=false){
+			array_push($lines,$mac[0]);
+			array_push($values,$lines);
+		}
 	}
 	echo json_encode($values);
 ?>
--- latency/test.php
+++ latency/test.php
@@ -1,6 +1,13 @@
 <?php
-$datetime = new DateTime( "now", new DateTimeZone( "+08:00" ) );
-
-    echo $datetime->format('H:i');
+	function dateDiff ($ins){
+		
+		$from=date_create(date('Y-m-d'));
+		$to=date_create($ins);
+		$to2=date_create(date_format($to,"Y-m-d"));
+		$diff=date_diff($to2,$from);
+		return $diff->format('%a');
+	}
+	echo 1+dateDiff("2023-09-05 09:22:09");
+	
 	
 ?>
